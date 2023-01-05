21333c29c4b1cf11f6de7a0c260d875f04ef9bd8
AndreyVasko <nate20@mail.ru>
2023-01-05 19:57:28 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str =n.toString().split('');
+    if(str[0] == '-') {
+        str = str.slice(1,str.length).reverse();
+        str.unshift('+');
+        return str.join('');
+    }
+      return +(n.toString().split('').reverse().join(''));
 }
